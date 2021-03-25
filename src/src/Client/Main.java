5155e5b49c140148689d90482325f221fb54b2ff
fsarabiar <f.sarabia.2019@alumnos.urjc.es>
2021-03-25 19:34:19 +0100

    Todo el login. Faltan cosas que todavía no se puede hacer. Hay alguna cosa  que no me convence. Revisarlo y me decis como lo veis.

---
+++ src/src/Client/Main.java
@@ -0,0 +1,14 @@
+package Client;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        LoginManage login = new LoginManage();
+        login.doOperation ();
+
+    }
+
+}
