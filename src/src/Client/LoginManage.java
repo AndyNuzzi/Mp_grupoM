f8516a02b854b6bf0d2c3abab8cdb2889522d5bd
Alessandro Nuzzi Herrero <sandro.nuzzi.h@gmail.com>
2021-03-25 09:31:40 +0100

    Actualización de LoginManage

--- src/src/Client/LoginManage.java
+++ src/src/Client/LoginManage.java
@@ -7,18 +7,32 @@ public class LoginManage {
 
     private void showLoginMenu() {
         System.out.println("");
-        System.out.println(" Wellcome ");
+        System.out.println(" Welcome ");
         System.out.println("");
         System.out.println(" Select an option");
-        System.out.println(" 1. Register as administrator");
+        System.out.println(" 1. Log in");
         System.out.println(" 2. Register as client");
         System.out.println("");
     }
 
     public void doOperation() {
-        this.showLoginMenu();
+        //Mostrará el menu y permitirá elegir entre registrarse o entrar con una cuenta ya creada
+        showLoginMenu();
         Scanner scanner = new Scanner(System.in);
         int loginOption = scanner.nextInt();
+        switch (loginOption) {
+            case 1: LogIn logIn = new LogIn();
+                break;
+            case 2: RegisterAsClient signIn = new RegisterAsClient();
+                break;
+            default:
+                System.out.println("This isn't a valid option");
+                //Añadir bucle para volver a pedir opcion?
+                break;
+        }
+        /*
+        He cambiado esto por lo de arriba
+
         if (loginOption == 1) {
             LogIn login = new RegisterAsAdministrator();
             login.doOperation();
@@ -29,6 +43,8 @@ public class LoginManage {
             System.out.println(" See you soon ");
             System.out.println("");
         }
+        */
+
 
     }
 }
