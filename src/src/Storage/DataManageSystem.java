a45a4e66956c0c3a64dbb080d4c1bc456fb177d7
Alessandro Nuzzi Herrero <sandro.nuzzi.h@gmail.com>
2021-03-25 10:17:54 +0100

    Actualización de DataManageSystem y FileOperations

--- src/src/Storage/DataManageSystem.java
+++ src/src/Storage/DataManageSystem.java
@@ -1,4 +1,37 @@
 package Storage;
 
 public class DataManageSystem {
+
+    //Properties
+    private DataManageSystem dataManageSystem;
+    private ClientFile clientFile;
+    private OfferFile offerFile;
+    private SaleFile saleFile;
+    private AdministratorFile administratorFile;
+    private StarshipFile starshipFile;
+    private CommentFile commentFile;
+    private PiratesFile piratesFile;
+    private SwindlerFile swindlerFile;
+    private UncheckedOffersFile uncheckedOffersFile;
+
+    //Methods
+    private DataManageSystem(){
+        clientFile = new ClientFile();
+        offerFile = new OfferFile();
+        saleFile = new SaleFile();
+        administratorFile = new AdministratorFile();
+        starshipFile = new StarshipFile();
+        commentFile = new CommentFile();
+        piratesFile = new PiratesFile();
+        swindlerFile = new SwindlerFile();
+        uncheckedOffersFile = new UncheckedOffersFile();
+    }
+
+    public DataManageSystem getInstance(){
+        if (dataManageSystem == null)
+            dataManageSystem = new DataManageSystem();
+        return dataManageSystem;
+    }
+
+
 }
--- src/src/Storage/FileOperations.java
+++ src/src/Storage/FileOperations.java
@@ -1,4 +1,32 @@
 package Storage;
 
-public class FileOperations {
+import java.io.*;
+import java.util.LinkedList;
+import java.util.List;
+
+public class FileOperations implements Serializable {
+
+
+    //Methods
+    public List read(String directory) {
+        try {
+            FileInputStream inputStream = new FileInputStream(directory);
+            ObjectInputStream input = new ObjectInputStream(inputStream);
+            return (List) input.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            return null;
+        }
+    }
+
+    public boolean write(String directory, List l) {
+        try {
+            FileOutputStream outputStream = new FileOutputStream(directory);
+            ObjectOutputStream output = new ObjectOutputStream(outputStream);
+            output.writeObject(l);
+            return true;
+        }
+        catch (IOException e) {
+            return false;
+        }
+    }
 }
