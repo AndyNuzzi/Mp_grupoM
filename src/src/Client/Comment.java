660f1bd50f355a7869f2679c68d67b6abae3324e
Andy <andy.nuzzi@gmail.com>
2021-04-01 11:50:53 +0200

    Comment es serializable

--- src/src/Client/Comment.java
+++ src/src/Client/Comment.java
@@ -1,6 +1,8 @@
 package Client;
 
-public class Comment {
+import java.io.Serializable;
+
+public class Comment implements Serializable {
     private String idSale;
     private String idClient;
     private String idSeller;
