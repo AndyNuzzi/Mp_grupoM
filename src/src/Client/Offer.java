b8c186c52ce7a267809f0841537340e69658ab22
Andy <andy.nuzzi@gmail.com>
2021-04-02 17:20:25 +0200

    sale y offer

--- src/src/Client/Offer.java
+++ src/src/Client/Offer.java
@@ -1,4 +1,77 @@
 package Client;
 
+import java.util.Date;
+import java.util.List;
+import Controller.*;
+
 public class Offer {
+
+    private List<String> starshipIdList;
+    private Date dateEnd;
+    private long price;
+    private String creator;
+    private String id;
+
+    public Offer() {
+    }
+
+    public List<String> getStarshipIdList() {
+        return starshipIdList;
+    }
+
+    public void setStarshipIdList(List<String> starshipIdList) {
+        this.starshipIdList = starshipIdList;
+    }
+
+    public Date getDateEnd() {
+        return dateEnd;
+    }
+
+    public void setDateEnd(Date dateEnd) {
+        this.dateEnd = dateEnd;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public void setPrice(long price) {
+        this.price = price;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public boolean addStarshipToOffer(Starship starship){
+        starshipIdList.add(starship.getRegisterNumber());
+        return true;
+    }
+
+    public boolean addStarshipToOffer(SpaceStation spaceStation){
+        starshipIdList.add(spaceStation.getRegisterNumber());
+        List <Starship> starshipList = spaceStation.getStarships();
+        for (Starship s:starshipList){
+            starshipIdList.add(s.getRegisterNumber());
+        }
+        return true;
+    }
+
+    public boolean finish(){
+
+        return addToUncheckedOffers();
+    }
+
 }
--- src/src/Client/Sale.java
+++ src/src/Client/Sale.java
@@ -1,5 +1,48 @@
 package Client;
 
+import java.util.Date;
+import Controller.*;
+
 public class Sale {
-// setters y getters
+
+    private Date date;
+    private Client seller;
+    private Client buyer;
+    private long cost;
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Client getSeller() {
+        return seller;
+    }
+
+    public void setSeller(Client seller) {
+        this.seller = seller;
+    }
+
+    public Client getBuyer() {
+        return buyer;
+    }
+
+    public void setBuyer(Client buyer) {
+        this.buyer = buyer;
+    }
+
+    public Long getCost() {
+        return cost;
+    }
+
+    public void setCost(Long cost) {
+        this.cost = cost;
+    }
+
+    public boolean buy(){
+        return storeBuy();
+    }
 }
