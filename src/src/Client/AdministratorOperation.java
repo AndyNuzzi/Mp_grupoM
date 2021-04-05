599f3abdedaa5cc418ef49b56954e41f609f9a24
fsarabiar <f.sarabia.2019@alumnos.urjc.es>
2021-04-05 17:27:02 +0200

    Clases Operation y Access

--- src/src/Client/AdministratorOperation.java
+++ src/src/Client/AdministratorOperation.java
@@ -4,4 +4,5 @@ import Controller.Controller;
 
 public abstract class AdministratorOperation implements Operation{
 
+
 }
--- src/src/Client/ClientOperation.java
+++ src/src/Client/ClientOperation.java
@@ -3,7 +3,6 @@ package Client;
 import Controller.*;
 
 public abstract class ClientOperation implements Operation{
-    protected Controller controller;
-    // intanciar el controller
+
 
 }
